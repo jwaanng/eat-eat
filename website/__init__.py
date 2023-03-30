54c35ba29450fcc35270fae28a63bde9674765f5
jessie wang <117032848+jwaanng@users.noreply.github.com>
2023-03-30 16:45:39 -0400

    html and css done for landing page, connected with flask

--- website/__init__.py
+++ website/__init__.py
@@ -3,7 +3,8 @@ from flask import Flask
 def create_app():
     """Function that is used to create Flask application object. Purpose of this function is to initialize the Flask application
     """
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="templates", static_folder="static")
+
     app.config['SECRET_KEY'] = 'deeznutsdeeznutsdeeznutsdeeznutsdeeznutsdeeznuts'
 
     from .views import views
--- website/views.py
+++ website/views.py
@@ -1,9 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 """Blueprint of our application. Has a set of urls for each page of this site"""
 
-views = Blueprint('views', __name__)
+views = Blueprint('views', __name__,template_folder='templates', static_folder='static')
 
 @views.route('/')
 def landing_page():
-    return "<hi> TEST</hi>"  # very basic
+    return render_template("base.html")
