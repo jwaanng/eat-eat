a327ecbd505418067abe9096f6c710ef53175acd
matkuang <100985622+matkuang@users.noreply.github.com>
2023-04-03 02:29:40 -0400

    Update tkintermapview_gui.py

--- tkintermapview_gui.py
+++ tkintermapview_gui.py
@@ -5,6 +5,16 @@ import tkintermapview as tkmap
 from classes import Restaurant
 
 
+def create_user_location_select_map(labelframe: tkinter.LabelFrame, width: int, height: int) -> tkmap.TkinterMapView:
+    map_widget = tkmap.TkinterMapView(labelframe, width=width, height=height)
+    map_widget.pack()
+    map_widget.set_tile_server("http://a.tile.stamen.com/toner/{z}/{x}/{y}.png")
+    map_widget.set_position(deg_x=43.6658971, deg_y=-79.3906104)
+    map_widget.set_zoom(15)
+
+    return map_widget
+
+
 def create_map_widget(labelframe: tkinter.LabelFrame, list_of_restaurant_paths: list[list[Restaurant]],
                       width: int, height: int) -> tkmap.TkinterMapView:
     """Create a map widget given a list of restaurant paths showing markers for each restaurant and the paths
@@ -37,7 +47,6 @@ def create_map_widget(labelframe: tkinter.LabelFrame, list_of_restaurant_paths: 
 
 
 if __name__ == '__main__':
-
     test = tkinter.Tk()
     test.title('Test')
     test.geometry('900x700')
@@ -45,10 +54,24 @@ if __name__ == '__main__':
     labelframe_test = tkinter.LabelFrame(test)
     labelframe_test.pack(pady=20)
 
-    example_restaurant_paths = [[Restaurant(identifier=1, coordinates=(43.6684, -79.38924),
-                                            name='Deli Shop', price=1, restaurant_type='Fast Food', address='Toronto'),
-                                 Restaurant(identifier=1, coordinates=(43.670420, -79.386450),
-                                            name='Starbucks', price=1, restaurant_type='Cafe', address='Toronto')]]
+    # example_restaurant_paths = [[Restaurant(identifier=1, coordinates=(43.6684, -79.38924),
+    #                                         name='Deli Shop', price=1, restaurant_type='Fast Food', address='Toronto'),
+    #                              Restaurant(identifier=1, coordinates=(43.670420, -79.386450),
+    #                                         name='Starbucks', price=1, restaurant_type='Cafe', address='Toronto')]]
 
-    map_widget_example = create_map_widget(labelframe_test, example_restaurant_paths, 800, 600)
+    # map_widget_example = create_map_widget(labelframe_test, example_restaurant_paths, 800, 600)
+    map_widget_example_2 = create_user_location_select_map(labelframe_test, 800, 600)
+    user_position = []
+
+    def add_marker_event(coords):
+        map_widget_example_2.delete_all_marker()
+        new_marker = map_widget_example_2.set_marker(coords[0], coords[1], text="You are here")
+        user_position.clear()
+        user_position.append(new_marker.position)
+
+
+    map_widget_example_2.add_right_click_menu_command(label="Select location",
+                                            command=add_marker_event,
+                                            pass_coords=True)
+
     test.mainloop()
